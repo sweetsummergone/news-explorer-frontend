a5e7026182742fad5cebbf0cb0a2dbf7dcc0b1ba
sweetsummergone <limfix.13@gmail.com>
2022-07-08 23:46:26 +0300

    Initial commit

---
+++ src/components/Main/Main.js
@@ -0,0 +1,5 @@
+export default function Main() {
+    return (
+        <h1 className="main__title">Welcome</h1>
+    )
+}
---
+++ src/components/SavedNews/SavedNews.js
@@ -0,0 +1,7 @@
+export default function SavedNews() {
+    return (
+        <article>
+            
+        </article>
+    )
+}
